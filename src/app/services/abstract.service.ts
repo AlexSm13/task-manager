be3a69510a71824c22f00b18b59f596db008adc9
AlexSm13 <79068122307@yandex.ru>
2020-03-21 20:53:32 +0500

    create methods for service

--- src/app/services/abstract.service.ts
+++ src/app/services/abstract.service.ts
@@ -1,9 +1,69 @@
 import { Injectable } from '@angular/core';
+import {of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbstractService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  private key = 'tasks';
+
+  getElements() {
+    return JSON.parse(localStorage.getItem(this.key)) || [];
+  }
+
+  saveElements(tasks) {
+    localStorage.setItem(this.key, JSON.stringify(tasks));
+  }
+
+  protected get() {
+    return of(this.getElements());
+  }
+
+  protected post(newTask) {
+
+    const tasks = this.getElements();
+
+    tasks.push(newTask);
+    this.saveElements(tasks);
+
+    return of(true);
+  }
+
+  protected put(updateTask) {
+
+    let tasks = this.getElements();
+
+    tasks = tasks.map(task => {
+      if (task.id === updateTask.id) {
+        task = updateTask;
+      }
+
+      return task;
+    });
+
+    this.saveElements(tasks);
+
+    return of(true);
+  }
+
+  protected delete(id) {
+
+    let tasks = this.getElements();
+
+    tasks = tasks.filter(task => {
+      return task.id !== id;
+    });
+
+    this.saveElements(tasks);
+
+    return of(true);
+  }
+
+  public removeAll() {
+    localStorage.removeItem(this.key);
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
 import {AbstractService} from './abstract.service';
+import {Observable} from 'rxjs';
+import {Task} from '../models/Task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService extends AbstractService {
 
-  constructor() {
-    super();
+  getTasks(): Observable<Task[]> {
+    return this.get();
+  }
+
+  saveTask(task: Task): Observable<boolean> {
+    return this.post(task);
+  }
+
+  updateTask(task: Task): Observable<boolean> {
+    return this.put(task);
+  }
+
+  deleteTask(id: number): Observable<boolean> {
+    return this.delete(id);
   }
 }
